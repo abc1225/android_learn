d74ba86ca9cdb51fbe295ac1e1843e03e74a6c41
your name <your email>
2024-04-03 17:31:20 +0800

    修改代码

---
+++ app/src/main/java/com/example/myapplication/CustomActionWebView.java
@@ -0,0 +1,192 @@
+package com.example.myapplication;
+
+
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by guoshuyu on 2017/6/16.
+ */
+
+public class CustomActionWebView extends WebView {
+
+    static String TAG = "CustomActionWebView";
+
+    ActionMode mActionMode;
+
+    List<String> mActionList = new ArrayList<>();
+
+    ActionSelectListener mActionSelectListener;
+
+    public interface ActionSelectListener {
+        void onClick(String title, String selectText);
+    }
+
+    public CustomActionWebView(Context context) {
+        super(context);
+    }
+
+    public CustomActionWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomActionWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    /**
+     * 处理item，处理点击
+     * @param actionMode
+     */
+    private ActionMode resolveActionMode(ActionMode actionMode) {
+        if (actionMode != null) {
+            final Menu menu = actionMode.getMenu();
+            mActionMode = actionMode;
+            menu.clear();
+            for (int i = 0; i < mActionList.size(); i++) {
+                menu.add(mActionList.get(i));
+            }
+            for (int i = 0; i < menu.size(); i++) {
+                MenuItem menuItem = menu.getItem(i);
+                menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        getSelectedData((String) item.getTitle());
+                        releaseAction();
+                        return true;
+                    }
+                });
+            }
+        }
+        mActionMode = actionMode;
+        return actionMode;
+    }
+
+    @Override
+    public ActionMode startActionMode(ActionMode.Callback callback) {
+        ActionMode actionMode = super.startActionMode(callback);
+        return resolveActionMode(actionMode);
+    }
+
+    @Override
+    public ActionMode startActionMode(ActionMode.Callback callback, int type) {
+        ActionMode actionMode = super.startActionMode(callback, type);
+        return resolveActionMode(actionMode);
+    }
+
+    private void releaseAction() {
+        if (mActionMode != null) {
+            mActionMode.finish();
+            mActionMode = null;
+        }
+    }
+
+    /**
+     * 点击的时候，获取网页中选择的文本，回掉到原生中的js接口
+     * @param title 传入点击的item文本，一起通过js返回给原生接口
+     */
+    private void getSelectedData(String title) {
+
+        Log.d("sloth", "click data: " + title);
+
+        String js = "(function getSelectedText() {" +
+                "var txt;" +
+                "var title = \"" + title + "\";" +
+                "if (window.getSelection) {" +
+                "txt = window.getSelection().toString();" +
+
+                "var range = window.getSelection().getRangeAt(0);" +
+                "var selectedText = range.extractContents();" +
+                "var span = document.createElement('span');" +
+                "span.style.color = 'red';" +
+                "span.appendChild(selectedText);" +
+                "range.insertNode(span);" +
+
+                "} else if (window.document.getSelection) {" +
+                "txt = window.document.getSelection().toString();" +
+
+                "var range = window.document.getSelection().getRangeAt(0);" +
+                "var selectedText = range.extractContents();" +
+                "var span = document.createElement('span');" +
+                "span.style.color = 'red';" +
+                "span.appendChild(selectedText);" +
+                "range.insertNode(span);" +
+
+                "} else if (window.document.selection) {" +
+                "txt = window.document.selection.createRange().text;" +
+
+                "var range = window.document.selection.getRangeAt(0);" +
+                "var selectedText = range.extractContents();" +
+                "var span = document.createElement('span');" +
+                "span.style.color = 'red';" +
+                "span.appendChild(selectedText);" +
+                "range.insertNode(span);" +
+
+                "}" +
+                "JSInterface.callback(txt,title);" +
+                "})()";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            evaluateJavascript("javascript:" + js, null);
+        } else {
+            loadUrl("javascript:" + js);
+        }
+    }
+
+    public void linkJSInterface() {
+        addJavascriptInterface(new ActionSelectInterface(this), "JSInterface");
+    }
+
+    /**
+     * 设置弹出action列表
+     * @param actionList
+     */
+    public void setActionList(List<String> actionList) {
+        mActionList = actionList;
+    }
+
+    /**
+     * 设置点击回掉
+     * @param actionSelectListener
+     */
+    public void setActionSelectListener(ActionSelectListener actionSelectListener) {
+        this.mActionSelectListener = actionSelectListener;
+    }
+
+    /**
+     * 隐藏消失Action
+     */
+    public void dismissAction() {
+        releaseAction();
+    }
+
+
+    /**
+     * js选中的回掉接口
+     */
+    private class ActionSelectInterface {
+
+        CustomActionWebView mContext;
+
+        ActionSelectInterface(CustomActionWebView c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void callback(final String value, final String title) {
+            if(mActionSelectListener != null) {
+                mActionSelectListener.onClick(title, value);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/WebViewActivity.java
@@ -0,0 +1,126 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebViewActivity extends AppCompatActivity {
+
+
+
+    View mLadingView;
+    CustomActionWebView mCustomActionWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        mLadingView = findViewById(R.id.loadingView);
+        mCustomActionWebView = (CustomActionWebView)findViewById(R.id.customActionWebView);
+
+
+        List<String> list = new ArrayList<>();
+        list.add("加红色");
+//        list.add("Item2");
+//        list.add("APIWeb");
+
+        mCustomActionWebView.setWebViewClient(new CustomWebViewClient());
+
+        //设置item
+        mCustomActionWebView.setActionList(list);
+
+        //链接js注入接口，使能选中返回数据
+        mCustomActionWebView.linkJSInterface();
+
+        mCustomActionWebView.getSettings().setBuiltInZoomControls(true);
+        mCustomActionWebView.getSettings().setDisplayZoomControls(false);
+        //使用javascript
+        mCustomActionWebView.getSettings().setJavaScriptEnabled(true);
+        mCustomActionWebView.getSettings().setDomStorageEnabled(true);
+
+
+        //增加点击回调
+        mCustomActionWebView.setActionSelectListener(new CustomActionWebView.ActionSelectListener() {
+            @Override
+            public void onClick(String title, String selectText) {
+                if(title.equals("APIWeb")) {
+                    Intent intent = new Intent(WebViewActivity.this, APIWebViewActivity.class);
+                    startActivity(intent);
+                    return;
+                }
+                Toast.makeText(WebViewActivity.this, "Click Item: " + title + "。\n\nValue: " + selectText, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //加载url
+        mCustomActionWebView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mCustomActionWebView.loadUrl("https://www.baidu.com/");
+            }
+        }, 200);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mCustomActionWebView != null) {
+            mCustomActionWebView.dismissAction();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+
+        private boolean mLastLoadFailed = false;
+
+        @Override
+        public void onPageFinished(WebView webView, String url) {
+            super.onPageFinished(webView, url);
+            if (!mLastLoadFailed) {
+                CustomActionWebView customActionWebView = (CustomActionWebView) webView;
+                customActionWebView.linkJSInterface();
+                mLadingView.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public void onPageStarted(WebView webView, String url, Bitmap favicon) {
+            super.onPageStarted(webView, url, favicon);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+//            super.onReceivedError(view, request, error);
+            mLastLoadFailed = true;
+            mLadingView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if(null != mCustomActionWebView && mCustomActionWebView.canGoBack()){
+            mCustomActionWebView.goBack();
+        }
+    }
+}
